01ee883cf0b301e03cfd81d51d14ce0bf4b91d56
Keks <keksrespekt@gmail.com>
2024-03-09 16:51:48 +0300

    адача3

---
+++ HW7.3/Program.cs
@@ -0,0 +1,20 @@
+﻿void ArrayFromEnd(int[] arr, int index)
+        {
+            if (index >= 0)
+            {
+                Console.Write($"{arr[index]}");
+                if (index > 0) 
+                {
+                    Console.Write(", "); 
+                }
+                ArrayFromEnd(arr, index - 1);
+            }
+            
+            
+        }
+            int[] myArray = { 77, 56, 3, -2, 11 };
+
+            Console.WriteLine("Элементы массива, начиная с конца:");
+            Console.Write("[");
+            ArrayFromEnd(myArray, myArray.Length - 1);
+             Console.Write("]");
