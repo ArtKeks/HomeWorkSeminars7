dfac52dc3b18cc4c5abe80f50ec886e34b7985a2
Keks <keksrespekt@gmail.com>
2024-03-09 16:06:13 +0300

    work1

---
+++ HW7.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+//  в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void ListNums(int n, int m)
+{
+    if (m >= n)
+    {
+        Console.Write($"{m} ");
+        return;
+    }
+    Console.Write($"{m} ");
+    ListNums(n, m + 1);
+}
+Console.Clear();
+Console.Write("Введите от какого числа вывести натуральные числа: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите до какого числа вывести натуральные числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ListNums(n - 1, m);
+//Для выдачи чисел включая последнее для передачи в функцию убрать "-1"
